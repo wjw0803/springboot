b8ffd8660e8ee6c3e745f61f470e549949a156e7
wjw <w18835460636@163.com>
2020-02-28 17:00:06 +0800

    完善springboot注解

--- src/main/java/com/dj/ssm/SpringbootReviewApplication.java
+++ src/main/java/com/dj/ssm/SpringbootReviewApplication.java
@@ -1,9 +1,13 @@
 package com.dj.ssm;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan("com.dj.ssm.mapper")
 public class SpringbootReviewApplication {
 
     public static void main(String[] args) {
