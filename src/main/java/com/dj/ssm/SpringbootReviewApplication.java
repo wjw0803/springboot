906b61e8f01f1c513dfd96e05da3fe7d29113a0d
wjw <w18835460636@163.com>
2020-02-28 16:59:05 +0800

    init commit

---
+++ src/main/java/com/dj/ssm/SpringbootReviewApplication.java
@@ -0,0 +1,13 @@
+package com.dj.ssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootReviewApplication.class, args);
+    }
+
+}
